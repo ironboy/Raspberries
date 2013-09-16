0072882cfea63acab1044ead97b8358600f039a0
Thomas Frank <thomas@nodebite.se>
2013-09-16 15:14:00 +0200

    Initial role playing properties...

--- raspberries/Program.cs
+++ raspberries/Program.cs
@@ -5,5 +5,10 @@
     {
         // Initial greeting
         Console.WriteLine("Hello your git!");
+
+
+        // Now let's build som arrays for calcs of roleplay strengts
+        int playerStrength = 5;
+        Console.Write(playerStrength);
     }
 }
